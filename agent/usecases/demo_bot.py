da49831560fed46f3f98f7a55f959765e2d22d1b
ggerber <gianandreadaniel.gerber@uzh.ch>
2025-10-18 15:10:02 +0200

    Refactor

--- agent/usecases/demo_bot.py
+++ agent/usecases/demo_bot.py
@@ -17,6 +17,11 @@ class Agent:
         self.speakeasy.login()  # This framework will help you log out automatically when the program terminates.
 
         self.g = Graph().parse("https://files.ifi.uzh.ch/ddis/teaching/2025/ATAI/dataset/graph.nt")
+        '''print(f"Graph has {len(self.g)} triples.\n")
+
+        for i, (s, p, o) in enumerate(self.g):
+            print(s, p, o)'''
+
         print('parsing done')
         self.speakeasy.register_callback(self.on_new_message, EventType.MESSAGE)
         self.speakeasy.register_callback(self.on_new_reaction, EventType.REACTION)
@@ -33,8 +38,7 @@ class Agent:
     def __execute_sparql(self, message: str, room: Chatroom):
         """Execute a SPARQL query."""
         try:
-            pattern = r"'''(.*?)'''"
-            match = re.search(pattern, message, re.DOTALL)
+            match = self.__extract_messages(message)
             if match:
                 query = match.group(1)
                 literals = self.__extract_literals(query)
@@ -50,6 +54,7 @@ class Agent:
             room.post_messages("There was no valid SPARQL query in your prompt, please encode the SPARQL query within single quotes or only provide the SPARQL query.")
 
     def __extract_literals(self, query: str):
+        """Extract literals from the  SPARQL query."""
         literals = []
         if len(query):
             for row in self.g.query(query):
@@ -57,6 +62,11 @@ class Agent:
                     if isinstance(value, Literal):
                         literals.append(value.toPython())
         return literals
+
+    def __extract_messages(self, message: str):
+        """Extract messages from the  SPARQL query."""
+        pattern = r"'''(.*?)'''"
+        return re.search(pattern, message, re.DOTALL)
 
     def on_new_reaction(self, reaction : str, message_ordinal : int, room : Chatroom):
         """Callback function to handle new reactions."""
